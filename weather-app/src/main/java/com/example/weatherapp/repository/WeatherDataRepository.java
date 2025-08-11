07524432569e1832a86bb3fd266af2a844d95da5
asadrathore6 <42371116+asadrathore6@users.noreply.github.com>
2025-08-11 14:32:35 +0400

    Revert "feat: Create Spring Boot app for weather data"

--- weather-app/src/main/java/com/example/weatherapp/repository/WeatherDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.weatherapp.repository;
-
-import com.example.weatherapp.entity.WeatherData;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WeatherDataRepository extends JpaRepository<WeatherData, Long> {
-}
